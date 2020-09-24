d60cca80f0985037a25737f7ae1d33a55bee3999
Saral Shrivastava <shrivastavaa278@gmail.com>
2020-09-24 22:39:23 +0530

    build

---
+++ build/precache-manifest.6696c4e7903f713a1c5a3956654d0b53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211bdd73c75288e5903c8e3ea540013a",
+    "url": "/Sorting-Algo-Visualizer/index.html"
+  },
+  {
+    "revision": "9490f9a1785fc22a70bd",
+    "url": "/Sorting-Algo-Visualizer/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0bc5ae773917bfba66bc",
+    "url": "/Sorting-Algo-Visualizer/static/css/main.44440cf7.chunk.css"
+  },
+  {
+    "revision": "9490f9a1785fc22a70bd",
+    "url": "/Sorting-Algo-Visualizer/static/js/2.02a6c56a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Sorting-Algo-Visualizer/static/js/2.02a6c56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc5ae773917bfba66bc",
+    "url": "/Sorting-Algo-Visualizer/static/js/main.6d6755b4.chunk.js"
+  },
+  {
+    "revision": "ac3b38c9e5cef3402b2d",
+    "url": "/Sorting-Algo-Visualizer/static/js/runtime-main.a0d73ab2.js"
+  }
+]);
